5f9d737ec4092fb35b892a20b6f63448fa261fd6
Juan Sebastián Rodriguez <rolo.velosa@gmail.com>
2026-01-21 04:21:41 -0500

    Modificacion de funciones y se agregaron nuevo campos en los DTO's

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,12 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.setGlobalPrefix('api/v1');
 
   app.useGlobalPipes(
